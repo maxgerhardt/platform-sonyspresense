8a69cece04ac2c6ee3deb9af80f7431786735fe1
unknown <maximilian.gerhardt@rub.de>
2022-08-05 15:19:41 +0200

    Create SPK file, fix uploader, add erase and bootloader target

--- builder/main.py
+++ builder/main.py
@@ -13,6 +13,9 @@
 # limitations under the License.
 
 import sys
+import subprocess
+import json
+import semantic_version
 from platform import system
 from os import makedirs
 from os.path import basename, isdir, join, isfile
@@ -21,6 +24,7 @@ from SCons.Script import (ARGUMENTS, COMMAND_LINE_TARGETS, AlwaysBuild,
                           Builder, Default, DefaultEnvironment)
 
 from platformio.util import get_serial_ports
+from platformio.package.version import get_original_version, pepver_to_semver
 
 
 def BeforeUpload(target, source, env):  # pylint: disable=W0613,W0621
@@ -58,10 +62,14 @@ env.Replace(
 
     ARFLAGS=["rc"],
 
-    SIZEPROGREGEXP=r"^(?:\.text|\.data|\.rodata|\.text.align|\.ARM.exidx|\.ccram_data)\s+(\d+).*",
+    SIZEPROGREGEXP=r"^(?:\.text|\.data|\.rodata|\.init_section|\.ARM.exidx)\s+(\d+).*",
     SIZEDATAREGEXP=r"^(?:\.data|\.bss|\.noinit)\s+(\d+).*",
     SIZECHECKCMD="$SIZETOOL -A -d $SOURCES",
     SIZEPRINTCMD='$SIZETOOL -B -d $SOURCES',
+    SPKSIZEPRINTCMD='%s %s "$SOURCES"' % (
+        '"$PYTHONEXE"',
+        '"%s"' % join(platform.get_package_dir("tool-spresense") or "", "getspkinfo", "src", "getspkinfo.py"),
+    ),
 
     PROGSUFFIX=".elf"
 )
@@ -69,6 +77,14 @@ env.Replace(
 # Allow user to override via pre:script
 if env.get("PROGNAME", "program") == "program":
     env.Replace(PROGNAME="firmware")
+
+# for referencing the right precompiled tool in spresense-tools
+os_folder_map = {
+    "Windows": "windows",
+    "Linux": "linux",
+    "Darwin": "macosx"
+}
+os_folder = os_folder_map.get(system(), "unknown_os")
 
 env.Append(
     BUILDERS=dict(
@@ -93,6 +109,17 @@ env.Append(
                 "$TARGET"
             ]), "Building $TARGET"),
             suffix=".hex"
+        ),
+        ElfToSpk=Builder(
+            action=env.VerboseAction(" ".join([
+                join(platform.get_package_dir("tool-spresense") or "", "mkspk", os_folder, "mkspk.exe" if system() == "Windows" else "mkspk"),
+                "-c",
+                "2",
+                "$SOURCES",
+                "$FLASH_NAME",
+                "$TARGET"
+            ]), "Building $TARGET"),
+            suffix=".spk"
         )
     )
 )
@@ -105,16 +132,66 @@ if not env.get("PIOFRAMEWORK"):
 #
 
 target_elf = None
+target_spk = None
 if "nobuild" in COMMAND_LINE_TARGETS:
     target_elf = join("$BUILD_DIR", "${PROGNAME}.elf")
     target_firm = join("$BUILD_DIR", "${PROGNAME}.bin")
+    target_spk = join("$BUILD_DIR", "${PROGNAME}.spk")
 else:
     target_elf = env.BuildProgram()
     target_firm = env.ElfToBin(join("$BUILD_DIR", "${PROGNAME}"), target_elf)
-    env.Depends(target_firm, "checkprogsize")
+    target_spk = env.ElfToSpk(join("$BUILD_DIR", "${PROGNAME}"), target_elf)
 
-AlwaysBuild(env.Alias("nobuild", target_firm))
-target_buildprog = env.Alias("buildprog", target_firm, target_firm)
+bootloader_actions = None
+erase_actions = None
+if "bootloader" in COMMAND_LINE_TARGETS:
+    SDK_DIR = platform.get_package_dir("tool-arduinosonyspresensesdk")
+    firmware_dir = join(SDK_DIR, "firmware")
+    fw_files = [
+        "loader.espk",
+        "gnssfw.espk",
+        "dnnrt-mp.espk",
+        "AESM.espk",
+        "sysutil.spk",
+    ]
+    bootloader_actions = [ 
+        env.VerboseAction(env.AutodetectUploadPort, "Looking for upload port..."),
+        env.VerboseAction(" ".join([
+            '"$PYTHONEXE"',
+            '"%s"' % join(platform.get_package_dir("tool-spresense") or "", "flash_writer", "scripts", "flash_writer.py"),
+            "-b", str(board.get("upload.speed", "115200")),
+            "-c", 
+            '"$UPLOAD_PORT"',
+            "-d",
+            "-n"] + ['"%s"' % join(firmware_dir, fw) for fw in fw_files]
+        ), "Burning bootloader")
+    ]
+if "erase" in COMMAND_LINE_TARGETS:
+    erase_targets = ["nuttx"] + ["sub" + str(i) for i in range(1, 6)]
+    erase_actions = [ 
+        env.VerboseAction(env.AutodetectUploadPort, "Looking for upload port..."),
+        env.VerboseAction(" ".join([
+            '"$PYTHONEXE"',
+            '"%s"' % join(platform.get_package_dir("tool-spresense") or "", "flash_writer", "scripts", "flash_writer.py"),
+            "-b", str(board.get("upload.speed", "115200")),
+            "-c", 
+            '"$UPLOAD_PORT"',
+            "-d",
+            "-n",
+            "-e", "nuttx",
+            "-e", "sub1",
+            "-e", "sub2",
+            "-e", "sub3",
+            "-e", "sub4",
+            "-e", "sub5",
+        ]), "Erasing flash")
+    ]
+
+env.AddPlatformTarget("bootloader", None, bootloader_actions, "Burn Bootloader")
+env.AddPlatformTarget("erase", None, erase_actions, "Erase")
+
+AlwaysBuild(env.Alias("nobuild", [target_firm, target_spk]))
+target_buildprog = env.Alias("buildprog", [target_firm, target_spk])
 
 #
 # Target: Print binary size
@@ -123,7 +200,12 @@ target_buildprog = env.Alias("buildprog", target_firm, target_firm)
 target_size = env.Alias(
     "size", target_elf,
     env.VerboseAction("$SIZEPRINTCMD", "Calculating size $SOURCE"))
+target_spk_size = env.Alias(
+    "spksize", target_spk,
+    env.VerboseAction("$SPKSIZEPRINTCMD", "Printing size $SOURCE"))
+env.Depends("checkprogsize", target_spk_size)
 AlwaysBuild(target_size)
+AlwaysBuild(target_spk_size)
 
 #
 # Target: Upload by default .bin file
@@ -168,17 +250,19 @@ elif upload_protocol == "serial":
 
     env.Replace(
         __configure_upload_port=__configure_upload_port,
-        UPLOADER=join(
-            '"%s"' % "to-be-replaced",
-            #'"%s"' % platform.get_package_dir("tool-stm32duino") or "",
-            "stm32flash", "stm32flash"),
+        UPLOADER=" ".join([
+            '"$PYTHONEXE"',
+            '"%s"' % join(platform.get_package_dir("tool-spresense") or "", "flash_writer", "scripts", "flash_writer.py"),
+            ]),
         UPLOADERFLAGS=[
-            "-g", board.get("upload.offset_address", "0x08000000"),
-            "-b", "115200", "-w"
+            "-s", 
+            "-b", board.get("upload.speed", "115200"),
+            "-d",
         ],
-        UPLOADCMD='$UPLOADER $UPLOADERFLAGS "$SOURCE" "${__configure_upload_port(__env__)}"'
+        UPLOADCMD='$UPLOADER $UPLOADERFLAGS -c "${__configure_upload_port(__env__)}" -n "$SOURCE"'
     )
 
+    upload_source = target_spk
     upload_actions = [
         env.VerboseAction(env.AutodetectUploadPort, "Looking for upload port..."),
         env.VerboseAction("$UPLOADCMD", "Uploading $SOURCE")
@@ -229,6 +313,59 @@ AlwaysBuild(env.Alias("upload", upload_source, upload_actions))
 if any("-Wl,-T" in f for f in env.get("LINKFLAGS", [])):
     print("Warning! '-Wl,-T' option for specifying linker scripts is deprecated. "
           "Please use 'board_build.ldscript' option in your 'platformio.ini' file.")
+
+#
+# Install wxPython dependency of tool-spresense
+#
+def install_python_deps():
+    def _get_installed_pip_packages():
+        result = {}
+        packages = {}
+        pip_output = subprocess.check_output(
+            [env.subst("$PYTHONEXE"), "-m", "pip", "list", "--format=json"]
+        )
+        pip_output = pip_output.decode('latin-1').strip()
+        if "[notice]" in pip_output:
+            ind = pip_output.index("[notice]")
+            pip_output = pip_output[:ind].strip()
+        try:
+            packages = json.loads(pip_output)
+        except Exception as exc:
+            print("Warning! Couldn't extract the list of installed Python packages.")
+            print("Output: " + str(pip_output))
+            print("Exception: " + repr(exc))
+            return {}
+        for p in packages:
+            result[p["name"]] = pepver_to_semver(p["version"])
+
+        return result
+
+    deps = {
+        "wxPython": ">=4.1.0",
+    }
+
+    installed_packages = _get_installed_pip_packages()
+    packages_to_install = []
+    for package, spec in deps.items():
+        if package not in installed_packages:
+            packages_to_install.append(package)
+        else:
+            version_spec = semantic_version.Spec(spec)
+            if not version_spec.match(installed_packages[package]):
+                packages_to_install.append(package)
+
+    if packages_to_install:
+        env.Execute(
+            env.VerboseAction(
+                (
+                    '"$PYTHONEXE" -m pip install -U --force-reinstall '
+                    + " ".join(['"%s%s"' % (p, deps[p]) for p in packages_to_install])
+                ),
+                "Installing ESP-IDF's Python dependencies",
+            )
+        )
+
+install_python_deps()
 
 #
 # Default targets
